c37227c9d6329c518823f43393667673a5dd7c36
Jan Štefankovič <67871935+autistukral@users.noreply.github.com>
2024-01-09 06:44:31 +0100

    update

--- tf2/script.js
+++ tf2/script.js
@@ -7,13 +7,16 @@ const engineer = document.querySelector('.c--engineer');
 const medic = document.querySelector('.c--medic');
 const sniper = document.querySelector('.c--sniper');
 const spy = document.querySelector('.c--spy');
+const list = document.querySelector('.classes--list');
 
 
 document.querySelector('.vishid--scout').addEventListener('click', () => {
     if (scout.classList.contains('visible')) {
         scout.classList.replace('visible', 'hidden');
+        list.classList.replace('l--hid', 'l--vis');
     } else {
         scout.classList.replace('hidden', 'visible');
+        list.classList.replace('l--vis', 'l--hid');
     };
     soldier.classList.replace('visible', 'hidden');
     pyro.classList.replace('visible', 'hidden');
@@ -28,8 +31,10 @@ document.querySelector('.vishid--scout').addEventListener('click', () => {
 document.querySelector('.vishid--soldier').addEventListener('click', () => {
     if (soldier.classList.contains('visible')) {
         soldier.classList.replace('visible', 'hidden');
+        list.classList.replace('l--hid', 'l--vis');
     } else {
         soldier.classList.replace('hidden', 'visible');
+        list.classList.replace('l--vis', 'l--hid');
     };
     scout.classList.replace('visible', 'hidden');
     pyro.classList.replace('visible', 'hidden');
@@ -44,8 +49,10 @@ document.querySelector('.vishid--soldier').addEventListener('click', () => {
 document.querySelector('.vishid--pyro').addEventListener('click', () => {
     if (pyro.classList.contains('visible')) {
         pyro.classList.replace('visible', 'hidden');
+        list.classList.replace('l--hid', 'l--vis');
     } else {
         pyro.classList.replace('hidden', 'visible');
+        list.classList.replace('l--vis', 'l--hid');
     };
     scout.classList.replace('visible', 'hidden');
     soldier.classList.replace('visible', 'hidden');
@@ -60,8 +67,10 @@ document.querySelector('.vishid--pyro').addEventListener('click', () => {
 document.querySelector('.vishid--demoman').addEventListener('click', () => {
     if (demoman.classList.contains('visible')) {
         demoman.classList.replace('visible', 'hidden');
+        list.classList.replace('l--hid', 'l--vis');
     } else {
         demoman.classList.replace('hidden', 'visible');
+        list.classList.replace('l--vis', 'l--hid');
     };
     scout.classList.replace('visible', 'hidden');
     soldier.classList.replace('visible', 'hidden');
@@ -76,8 +85,10 @@ document.querySelector('.vishid--demoman').addEventListener('click', () => {
 document.querySelector('.vishid--heavy').addEventListener('click', () => {
     if (heavy.classList.contains('visible')) {
         heavy.classList.replace('visible', 'hidden');
+        list.classList.replace('l--hid', 'l--vis');
     } else {
         heavy.classList.replace('hidden', 'visible');
+        list.classList.replace('l--vis', 'l--hid');
     };
     scout.classList.replace('visible', 'hidden');
     soldier.classList.replace('visible', 'hidden');
@@ -92,8 +103,10 @@ document.querySelector('.vishid--heavy').addEventListener('click', () => {
 document.querySelector('.vishid--engineer').addEventListener('click', () => {
     if (engineer.classList.contains('visible')) {
         engineer.classList.replace('visible', 'hidden');
+        list.classList.replace('l--hid', 'l--vis');
     } else {
         engineer.classList.replace('hidden', 'visible');
+        list.classList.replace('l--vis', 'l--hid');
     };
     scout.classList.replace('visible', 'hidden');
     soldier.classList.replace('visible', 'hidden');
@@ -108,8 +121,10 @@ document.querySelector('.vishid--engineer').addEventListener('click', () => {
 document.querySelector('.vishid--medic').addEventListener('click', () => {
     if (medic.classList.contains('visible')) {
         medic.classList.replace('visible', 'hidden');
+        list.classList.replace('l--hid', 'l--vis');
     } else {
         medic.classList.replace('hidden', 'visible');
+        list.classList.replace('l--vis', 'l--hid');
     };
     scout.classList.replace('visible', 'hidden');
     soldier.classList.replace('visible', 'hidden');
@@ -124,8 +139,10 @@ document.querySelector('.vishid--medic').addEventListener('click', () => {
 document.querySelector('.vishid--sniper').addEventListener('click', () => {
     if (sniper.classList.contains('visible')) {
         sniper.classList.replace('visible', 'hidden');
+        list.classList.replace('l--hid', 'l--vis');
     } else {
         sniper.classList.replace('hidden', 'visible');
+        list.classList.replace('l--vis', 'l--hid');
     };
     scout.classList.replace('visible', 'hidden');
     soldier.classList.replace('visible', 'hidden');
@@ -140,8 +157,10 @@ document.querySelector('.vishid--sniper').addEventListener('click', () => {
 document.querySelector('.vishid--spy').addEventListener('click', () => {
     if (spy.classList.contains('visible')) {
         spy.classList.replace('visible', 'hidden');
+        list.classList.replace('l--hid', 'l--vis');
     } else {
         spy.classList.replace('hidden', 'visible');
+        list.classList.replace('l--vis', 'l--hid');
     };
     scout.classList.replace('visible', 'hidden');
     soldier.classList.replace('visible', 'hidden');
