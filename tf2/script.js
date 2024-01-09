5ed484d549e4bab985dcc089334c9d3bc6a34e8f
Jan Štefankovič <67871935+autistukral@users.noreply.github.com>
2024-01-09 05:44:08 +0100

    tf2

---
+++ tf2/script.js
@@ -0,0 +1,159 @@
+const scout = document.querySelector('.c--scout');
+const soldier = document.querySelector('.c--soldier');
+const pyro = document.querySelector('.c--pyro');
+const demoman = document.querySelector('.c--demoman');
+const heavy = document.querySelector('.c--heavy');
+const engineer = document.querySelector('.c--engineer');
+const medic = document.querySelector('.c--medic');
+const sniper = document.querySelector('.c--sniper');
+const spy = document.querySelector('.c--spy');
+
+
+document.querySelector('.vishid--scout').addEventListener('click', () => {
+    if (scout.classList.contains('visible')) {
+        scout.classList.replace('visible', 'hidden');
+    } else {
+        scout.classList.replace('hidden', 'visible');
+    };
+    soldier.classList.replace('visible', 'hidden');
+    pyro.classList.replace('visible', 'hidden');
+    demoman.classList.replace('visible', 'hidden');
+    heavy.classList.replace('visible', 'hidden');
+    engineer.classList.replace('visible', 'hidden');
+    medic.classList.replace('visible', 'hidden');
+    sniper.classList.replace('visible', 'hidden');
+    spy.classList.replace('visible', 'hidden');
+});
+
+document.querySelector('.vishid--soldier').addEventListener('click', () => {
+    if (soldier.classList.contains('visible')) {
+        soldier.classList.replace('visible', 'hidden');
+    } else {
+        soldier.classList.replace('hidden', 'visible');
+    };
+    scout.classList.replace('visible', 'hidden');
+    pyro.classList.replace('visible', 'hidden');
+    demoman.classList.replace('visible', 'hidden');
+    heavy.classList.replace('visible', 'hidden');
+    engineer.classList.replace('visible', 'hidden');
+    medic.classList.replace('visible', 'hidden');
+    sniper.classList.replace('visible', 'hidden');
+    spy.classList.replace('visible', 'hidden');
+});
+
+document.querySelector('.vishid--pyro').addEventListener('click', () => {
+    if (pyro.classList.contains('visible')) {
+        pyro.classList.replace('visible', 'hidden');
+    } else {
+        pyro.classList.replace('hidden', 'visible');
+    };
+    scout.classList.replace('visible', 'hidden');
+    soldier.classList.replace('visible', 'hidden');
+    demoman.classList.replace('visible', 'hidden');
+    heavy.classList.replace('visible', 'hidden');
+    engineer.classList.replace('visible', 'hidden');
+    medic.classList.replace('visible', 'hidden');
+    sniper.classList.replace('visible', 'hidden');
+    spy.classList.replace('visible', 'hidden');
+});
+
+document.querySelector('.vishid--demoman').addEventListener('click', () => {
+    if (demoman.classList.contains('visible')) {
+        demoman.classList.replace('visible', 'hidden');
+    } else {
+        demoman.classList.replace('hidden', 'visible');
+    };
+    scout.classList.replace('visible', 'hidden');
+    soldier.classList.replace('visible', 'hidden');
+    pyro.classList.replace('visible', 'hidden');
+    heavy.classList.replace('visible', 'hidden');
+    engineer.classList.replace('visible', 'hidden');
+    medic.classList.replace('visible', 'hidden');
+    sniper.classList.replace('visible', 'hidden');
+    spy.classList.replace('visible', 'hidden');
+});
+
+document.querySelector('.vishid--heavy').addEventListener('click', () => {
+    if (heavy.classList.contains('visible')) {
+        heavy.classList.replace('visible', 'hidden');
+    } else {
+        heavy.classList.replace('hidden', 'visible');
+    };
+    scout.classList.replace('visible', 'hidden');
+    soldier.classList.replace('visible', 'hidden');
+    pyro.classList.replace('visible', 'hidden');
+    demoman.classList.replace('visible', 'hidden');
+    engineer.classList.replace('visible', 'hidden');
+    medic.classList.replace('visible', 'hidden');
+    sniper.classList.replace('visible', 'hidden');
+    spy.classList.replace('visible', 'hidden');
+});
+
+document.querySelector('.vishid--engineer').addEventListener('click', () => {
+    if (engineer.classList.contains('visible')) {
+        engineer.classList.replace('visible', 'hidden');
+    } else {
+        engineer.classList.replace('hidden', 'visible');
+    };
+    scout.classList.replace('visible', 'hidden');
+    soldier.classList.replace('visible', 'hidden');
+    pyro.classList.replace('visible', 'hidden');
+    demoman.classList.replace('visible', 'hidden');
+    heavy.classList.replace('visible', 'hidden');
+    medic.classList.replace('visible', 'hidden');
+    sniper.classList.replace('visible', 'hidden');
+    spy.classList.replace('visible', 'hidden');
+});
+
+document.querySelector('.vishid--medic').addEventListener('click', () => {
+    if (medic.classList.contains('visible')) {
+        medic.classList.replace('visible', 'hidden');
+    } else {
+        medic.classList.replace('hidden', 'visible');
+    };
+    scout.classList.replace('visible', 'hidden');
+    soldier.classList.replace('visible', 'hidden');
+    pyro.classList.replace('visible', 'hidden');
+    demoman.classList.replace('visible', 'hidden');
+    heavy.classList.replace('visible', 'hidden');
+    engineer.classList.replace('visible', 'hidden');
+    sniper.classList.replace('visible', 'hidden');
+    spy.classList.replace('visible', 'hidden');
+});
+
+document.querySelector('.vishid--sniper').addEventListener('click', () => {
+    if (sniper.classList.contains('visible')) {
+        sniper.classList.replace('visible', 'hidden');
+    } else {
+        sniper.classList.replace('hidden', 'visible');
+    };
+    scout.classList.replace('visible', 'hidden');
+    soldier.classList.replace('visible', 'hidden');
+    pyro.classList.replace('visible', 'hidden');
+    demoman.classList.replace('visible', 'hidden');
+    heavy.classList.replace('visible', 'hidden');
+    engineer.classList.replace('visible', 'hidden');
+    medic.classList.replace('visible', 'hidden');
+    spy.classList.replace('visible', 'hidden');
+});
+
+document.querySelector('.vishid--spy').addEventListener('click', () => {
+    if (spy.classList.contains('visible')) {
+        spy.classList.replace('visible', 'hidden');
+    } else {
+        spy.classList.replace('hidden', 'visible');
+    };
+    scout.classList.replace('visible', 'hidden');
+    soldier.classList.replace('visible', 'hidden');
+    pyro.classList.replace('visible', 'hidden');
+    demoman.classList.replace('visible', 'hidden');
+    heavy.classList.replace('visible', 'hidden');
+    engineer.classList.replace('visible', 'hidden');
+    medic.classList.replace('visible', 'hidden');
+    sniper.classList.replace('visible', 'hidden');
+});
+
+
+
+
+
