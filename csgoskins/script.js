36a4a7844b590a4c9dd7294adf04179941b6d991
Jan Štefankovič <67871935+autistukral@users.noreply.github.com>
2024-01-27 04:47:26 +0100

    big update

--- csgoskins/script.js
+++ csgoskins/script.js
@@ -11,10 +11,55 @@ document.querySelectorAll('.ownBtn').forEach(ownButton => ownButton.addEventList
     }
 }));
 
+// stop scrolling vars
+var keys = { 32: 1, 33: 1, 34: 1, 35: 1, 36: 1, 37: 1, 38: 1, 39: 1, 40: 1 };
+
+// disable / enable scrolling
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch (e) { }
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// image zoom with scroll disable/enable
+
 document.querySelectorAll('.skins_img').forEach(imgButton => imgButton.addEventListener('click', () => {
     if (imgButton.classList.contains('img_normal')) {
-        imgButton.classList.replace('img_normal', 'img_zoomed')
+        imgButton.classList.replace('img_normal', 'img_zoomed');
+        disableScroll();
+        imgButton.src = imgButton.src.replace('low_', 'high_');
     } else {
-        imgButton.classList.replace('img_zoomed', 'img_normal')
+        imgButton.classList.replace('img_zoomed', 'img_normal');
+        enableScroll();
+        imgButton.src = imgButton.src.replace('high_', 'low_');
     }
 }))
