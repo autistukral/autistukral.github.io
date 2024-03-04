e1f1f8d2654527924b35cbb607129e2f8d54e5ea
Jan Štefankovič <67871935+autistukral@users.noreply.github.com>
2024-03-04 22:09:24 +0100

    u

--- webp.ps1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem |`
-    Where-Object { $_.Extension -eq ".jpg" -or $_.Extension -eq ".png" } |`
-    ForEach-Object { Invoke-Expression ("cwebp -q 80 " + $_.Name + " -o " + $_.BaseName + ".webp") }
-pause
